93c74915ef5c4eee134dcbce57f00978b184812b
eozmert <en-es111@hotmail.com>
2023-02-25 13:15:03 +0300

    category edit added

--- src/main/java/com/opensource/blogecole/dataAccess/abstracts/CategoryDao.java
+++ src/main/java/com/opensource/blogecole/dataAccess/abstracts/CategoryDao.java
@@ -4,6 +4,9 @@ import com.opensource.blogecole.entities.concretes.Category;
 import com.opensource.blogecole.entities.concretes.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface CategoryDao extends JpaRepository<Category, Integer> {
     Category findById(int id);
+    List<Category> findAllByName(String name);
 }
